c1753307fd1a672cccdbbf1c98adae6b9dcd46c9
SotaSill <sotasill@gmail.com>
2024-10-28 00:55:20 +0200

    7

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,4 +1,4 @@
-import css from "./Searchbox.module.css";
+import css from "./SearchBox.module.css";
 import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 import { useDispatch, useSelector } from "react-redux";
 
